b71ad9cbe404e148d016a6a3e82fc6777b31472e
Bogdan <bogdan-747@mail.ru>
2024-09-08 14:22:36 +0300

    java_project

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.lang.reflect.Array;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.List;
+import java.util.Scanner;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double firstNumber = Integer.parseInt(scanner.next());
+        System.out.println("first number: " + firstNumber);
+
+    }
+
+}
+
